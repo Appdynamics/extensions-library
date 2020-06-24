305d3a87ddf284224d77770d3203bc4f003b2058
Adam Leftik <aleftik@gmail.com>
2020-06-24 10:27:55 -0700

    adding config statics

---
+++ src/main/java/com/appdynamics/extensions/common/rest/RESTConfig.java
@@ -0,0 +1,14 @@
+package com.appdynamics.extensions.common.rest;
+
+public class RESTConfig {
+    public static final String EVENT_SERVICE_URL = "events-url";
+    public static final String DNAC_URL = "dnac-url";
+    public static final String DNAC_USER = "dnac-user";
+    public static final String DNAC_PASSWORD = "dnac-password";
+    public static final String GLOBAL_ACCOUNT_KEY = "global-account-key";
+    public static final String EVENT_SERVICE_API_KEY = "ontroller-event-api-key";
+    public static final String PORT = "port";
+    public static final String CONTROLLER_API_KEY = "controller-api-key";
+    public static final String CONTROLLER_URL = "controller-url";
+    public static final String THOUSAND_EYES_API_KEY = "te-api-key";
+}
