163986840f704227177f724ed9914f679b4503c2
Adam Leftik <aleftik@gmail.com>
2020-06-30 09:55:26 -0700

    Update RESTConfig.java

--- src/main/java/com/appdynamics/extensions/common/rest/RESTConfig.java
+++ src/main/java/com/appdynamics/extensions/common/rest/RESTConfig.java
@@ -6,7 +6,7 @@ public class RESTConfig {
     public static final String DNAC_USER = "dnac-user";
     public static final String DNAC_PASSWORD = "dnac-password";
     public static final String GLOBAL_ACCOUNT_KEY = "global-account-key";
-    public static final String EVENT_SERVICE_API_KEY = "ontroller-event-api-key";
+    public static final String EVENT_SERVICE_API_KEY = "controller-event-api-key";
     public static final String PORT = "port";
     public static final String CONTROLLER_API_KEY = "controller-api-key";
     public static final String CONTROLLER_URL = "controller-url";
